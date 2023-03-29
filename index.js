7140a42f83b75974482e820f8707aeaa4be4c68a
odaney baker <bakerobtb@gmail.com>
2023-03-29 18:00:19 -0400

    Done

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const divide = function() {
+  return 2000 / 100;
+}
+console.log(divide()); 
+
+const square = (num) => {
+  return num * num;
+}
+console.log(square(5)); 
+
+const add = (num1, num2) => {
+  return num1 + num2;
+}
+console.log(add(10,20)); 
